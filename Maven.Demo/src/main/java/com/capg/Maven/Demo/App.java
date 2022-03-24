a003962942baaf21485d74713b81b307359116d7
Nageswari3 <nageswari.0246@gmail.com>
2022-03-24 15:08:06 +0530

    commited

---
+++ Maven.Demo/src/main/java/com/capg/Maven/Demo/App.java
@@ -0,0 +1,12 @@
+package com.capg.Maven.Demo;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
